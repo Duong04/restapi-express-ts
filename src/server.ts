771c964fd8e3b868f1fb1b4086762085fd789254
unknown <tinhabc3009@gmail.com>
2026-03-06 14:07:38 +0700

    .

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,6 @@ import connectDB from '~/config/database'
 
 const PORT = process.env.PORT || 3000
 dotenv.config()
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
 
 const startServer = async () => {
   await connectDB()
